c8c021e2a0f1fa0478bc0ccf5e7e0db4f0ed81cc
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-06-29 05:00:46 +0530

    web speech api

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const result = document.querySelector('.tetxarea');
+const btn = document.querySelector('.btn');
+const copyBtn = document.querySelector('#copyBtn');
+const speechRecognition = window.webkitSpeechRecognition;
+const recognition = new speechRecognition();
+
+// recognition.continuous = true;
+
+recognition.onresult = (event) => {
+    result.innerHTML = event.results[0][0]['transcript']
+}
+
+recognition.onstart = () => {
+    btn.innerHTML = 'Recording...'
+}
+
+recognition.onspeechend = () => {
+    btn.innerHTML = 'Recording Ended.'
+    setTimeout(() => {
+        btn.innerHTML = 'Record'
+    },2000);
+}
+
+recognition.onerror = () => {
+    btn.innerHTML = 'Some error occured! try again.'
+    setTimeout(() => {
+        btn.innerHTML = 'Record'
+    },2000);
+}
+
+
+btn.addEventListener('click', () => {
+    recognition.start();
+});
+
+function copyText() {
+    const copyText = result;
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    copyBtn.innerText = 'Copied!!'
+    setTimeout(() => {
+        copyBtn.innerHTML = 'Copy Text'
+    },3000);
+}
