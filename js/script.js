eea6a1be83c4791556cde14383563f4fa52ebf2f
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-06-29 13:52:30 +0530

    database array added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////////////
+//      databaase
+////////////////////////////////////////////////////////////////////////////////////
+var quizObjList = new Array();
+
+/////////////////////////////////////////////////////////////////////////////////////
+//      Object
+/////////////////////////////////////////////////////////////////////////////////////
+function Quiz () {
+    var __id = id;
+    var __question = question;
+    var __correctAnswer = correctAnswer;
+    var __correctAnswerSyntaxes = correctAnswerSyntaxes;
+    var __userAnswer = userAnswer;
+    var __userAnswerMarks = userAnswerMarks;
+
+    this.getId = function () {
+        return __id;
+    }
+    this.getQuestion = function () {
+        return __question;
+    }
+    this.getCorrectAnswer = function () {
+        return __correctAnswer;
+    }
+    this.getCorrectAnswerSyntaxes = function () {
+        return __correctAnswerSyntaxes;
+    }
+    this.getUserAnswer = function () {
+        return __userAnswer;
+    }
+    this.getUserAnswerMarks = function () {
+        return __userAnswerMarks;
+    }
+
+    this.setId = function (newid) {
+        __id = newid;
+    }
+    this.setQuestion = function (newquestion) {
+        __question = newquestion;
+    }
+    this.setCorrectAnswer = function (newcorrectAnswer) {
+        __correctAnswer = newcorrectAnswer;
+    }
+    this.setCorrectAnswerSyntaxes = function (newcorrectAnswerSyntaxes) {
+        __correctAnswerSyntaxes = newcorrectAnswerSyntaxes;
+    }
+    this.setUserAnswer = function (newuserAnswer) {
+        __userAnswer = newuserAnswer;
+    }
+    this.setUserAnswerMarks = function (newuserAnswerMarks) {
+        __userAnswerMarks = newuserAnswerMarks;
+    }
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//      Database Initialise
+////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//      Bussiness logics
+////////////////////////////////////////////////////////////////////////////////////
+
 const result = document.querySelector('.tetxarea');
 const btn = document.querySelector('.btn');
 const copyBtn = document.querySelector('#copyBtn');
 const speechRecognition = window.webkitSpeechRecognition;
 const recognition = new speechRecognition();
-
-// recognition.continuous = true;
 
 recognition.onresult = (event) => {
     console.log(event.results[0][0]['transcript'])
