eaf6216779e2bb52b1597fe889651cb895d5ea10
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-06-30 11:27:46 +0530

    implementing js

--- js/script.js
+++ js/script.js
@@ -57,13 +57,71 @@ function Quiz (id , question , correctAnswer , correctAnswerSyntaxes , userAnswe
 /////////////////////////////////////////////////////////////////////////////////////
 //      Database Initialise
 ////////////////////////////////////////////////////////////////////////////////////
+
 let quiz1 = new Quiz(1,'What is your pet?', 'dog' ,'kiri laki', '', 0);
+quizObjList.push(quiz1);
+
+let quiz2 = new Quiz(1,'What is your car?', 'toyota' ,'toyota hilux', '', 0);
+quizObjList.push(quiz2);
+
+const questionsAndAnswers = [
+    {
+        question: "What is Networks?",
+        answer: "Networks refer to a group of interconnected devices or systems that can communicate and share resources with each other.",
+    },
+    {
+        question: "What is Networks?",
+        answer: "Networks refer to a group of interconnected devices or systems that can communicate and share resources with each other.",
+    },
+    // {
+    //     question: "What is Networks?",
+    //     answer: "Networks refer to a group of interconnected devices or systems that can communicate and share resources with each other.",
+    // },
+    // {
+    //     question: "What is Networks?",
+    //     answer: "Networks refer to a group of interconnected devices or systems that can communicate and share resources with each other.",
+    // },
+    // {
+    //     question: "What is Networks?",
+    //     answer: "Networks refer to a group of interconnected devices or systems that can communicate and share resources with each other.",
+    // },
+    {
+        question: "What is HTML?",
+        answer: "HTML stands for HyperText Markup Language, which is the standard markup language for creating web pages.",
+    },
+    // Add more questions and answers as needed
+];
+
+// Generate the HTML code
+let htmlCode = "";
+questionsAndAnswers.forEach((qa, index) => {
+    $(`
+    <div class="fp-con-cnt-cc">
+      <div>
+        <button type="button" class="btn btn-secondary">🔊 Question</button>
+        <button type="button" class="btn btn-success" style="margin-left: 20px" id="btn">🎙 Answer</button>
+        <button type="button" class="btn btn-primary" style="margin-left: 20px" id="submit">👉 Submit</button>
+      </div>
+      <div>
+        <label class="form-label" style="color: white" id="lblQuestion">${index + 1}) ${qa.question}</label>
+      </div>
+      <div>
+        <input class="form-control" id="txtareaUserAnswer" placeholder="Type here answer..." style="width: 500px">
+        <label for="txtareaUserAnswer"></label>
+      </div>
+    </div>
+  `).appendTo('.fp-con-cnt');
+});
+
+// Output the generated HTML code
+console.log(htmlCode);
+
 
 /////////////////////////////////////////////////////////////////////////////////////
 //      Bussiness logics
 ////////////////////////////////////////////////////////////////////////////////////
 
-const result = document.querySelector('#tetxarea');
+const result = document.querySelector('#txtareaUserAnswer');
 const btn = document.querySelector('#btn');
 const copyBtn = document.querySelector('#copyBtn');
 const speechRecognition = window.webkitSpeechRecognition;
